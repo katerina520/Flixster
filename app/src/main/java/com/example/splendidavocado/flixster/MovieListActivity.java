3c35c93f776d5725689a8f53ae1c9614ae094b58
Ekaterina Korovina <splendidavocado@fb.com>
2018-06-27 15:04:51 -0700

    commiting in the process cause I suspect my laptop will die

--- app/src/main/java/com/example/splendidavocado/flixster/MovieListActivity.java
+++ app/src/main/java/com/example/splendidavocado/flixster/MovieListActivity.java
@@ -2,9 +2,18 @@ package com.example.splendidavocado.flixster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MovieListActivity extends AppCompatActivity {
 
@@ -17,10 +26,16 @@ public class MovieListActivity extends AppCompatActivity {
     // the API key -- TODO move
     public final static String API_KEY = "f5c1e23f995bf777239c87cf84ae41f8";
 
+    public final static String TAG = "MovieActivity";
+
 
     // instance fields
     AsyncHttpClient client;
 
+    // the base url for loading images
+    String imageBaseUrl;
+    // the poster size to use when fetching images, part of the url
+    String posterSize;
 
 
 
@@ -30,6 +45,8 @@ public class MovieListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_list);
         // initialize client
         client = new AsyncHttpClient();
+
+        getConfiguration();
     }
     // get the configuration form the API
     private void getConfiguration() {
@@ -38,6 +55,49 @@ public class MovieListActivity extends AppCompatActivity {
         // set the request parameters
         RequestParams params = new RequestParams();
         params.put(API_KEY_PARAM, API_KEY);
-        // execute a GET request
+        // execute a GET request expecting a JSON object response
+        client.get(url, params, new JsonHttpResponseHandler()
+        {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONObject images = response.getJSONObject("images");
+
+                    // get image base url
+                    imageBaseUrl = images.getString("secure_base_url");
+                    // get the pster size
+                    JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
+                    // use the option at index 3 or w342 as a fallback
+                    posterSize = posterSizeOptions.optString(3, "w342");
+
+
+                } catch (JSONException e)
+                {
+                    logError("Failed parsing configuration", e, true);
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+    }
+
+    // handle errors, log and alert
+    private void logError(String message, Throwable error, boolean alertUser)
+    {
+        Log.e(TAG, message, error);
+        // alert the user to avoid silent errors
+        if (alertUser)
+        {
+            // show a long toast with the error message
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+        }
+
     }
 }
